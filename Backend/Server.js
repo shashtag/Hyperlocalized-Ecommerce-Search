b4a1869b3917db7d0554c31ee2c339976457f985
Shashwat Gupta <shashwatsatna@gmail.com>
2021-07-25 15:59:27 +0530

    ✨ mongoose

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
-import search from "./Api/search.route.js";
+import product from "./Routes/product.route.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/search", search);
+app.use("/api/product", product);
 app.use("*", (req, res) => res.status(404).json({ error: "Route not found" }));
 
 export default app;
